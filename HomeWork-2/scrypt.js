4ace51e4500627bab06906ac8ffbb05fab3b9b02
ImDyA <vasurman13@gmail.com>
2021-08-18 23:21:25 +0300

    home work 2 version 3

--- HomeWork-2/scrypt.js
+++ HomeWork-2/scrypt.js
@@ -12,4 +12,39 @@ const styleJS = (element, text) => {
  invisibleElement.innerText = text;
 }
 
-styleJS(button, "test");
+// styleJS(button, "test");
+
+
+// console.log(button.getBoundingClientRect());
+// console.log(button.getClientRects());
+
+
+
+const styleJS2 = (elem, text) => {
+    const invisibleElement = document.createElement("p");
+    invisibleElement.innerText = text;
+    invisibleElement.classList.add("invisible");
+
+    const elemStyle = elem.getBoundingClientRect();
+
+    invisibleElement.style.bottom = elemStyle.bottom + "px";
+    invisibleElement.style.height = elemStyle.height + "px";
+    invisibleElement.style.left = elemStyle.left + "px";
+    invisibleElement.style.right = elemStyle.right + "px";
+    invisibleElement.style.top = elemStyle.top + "px";
+    invisibleElement.style.width = elemStyle.width + "px";
+    invisibleElement.style.x = elemStyle.x + "px";
+    invisibleElement.style.y = elemStyle.y + "px";
+
+    elem.before(invisibleElement);
+
+    // console.log(elemStyle.left, invisibleElement.style.left);
+    // console.log(elemStyle);
+    // console.log(button.getBoundingClientRect());
+}
+
+
+styleJS2(button, "test 2");
+
+console.log(document.querySelector("p").getBoundingClientRect());
+console.log(button.getBoundingClientRect());
