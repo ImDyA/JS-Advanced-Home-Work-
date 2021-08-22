9931347fdd0b636a36a5213adb8c27773f1b263b
ImDyA <vasurman13@gmail.com>
2021-08-22 13:38:31 +0300

    HomeWork-9 not advanced

---
+++ HomeWork-9 not advanced/script.js
@@ -0,0 +1,64 @@
+const data = [
+    {
+      name: "John",
+      age: 24,
+      position: "senior",
+      isActive: false,
+    },
+    {
+      name: "Peter",
+      age: 33,
+      position: "middle",
+      isActive: false,
+    },
+    {
+      name: "Sam",
+      age: 29,
+      position: "junior",
+      isActive: true,
+    },
+    {
+      name: "Mary",
+      age: 24,
+      position: "middle",
+      isActive: false,
+    },
+    {
+      name: "Steve",
+      age: 23,
+      position: "middle",
+      isActive: true,
+    },
+    {
+      name: "Kate",
+      age: 31,
+      position: "middle",
+      isActive: false,
+    },
+    {
+      name: "Sally",
+      age: 19,
+      position: "junior",
+      isActive: false,
+    },
+    {
+      name: "Jack",
+      age: 19,
+      position: "middle",
+      isActive: true,
+    },
+  ];
+
+  massObj = (mass, obj) => {
+    let finalMass = mass.filter(elem => {
+        for(let constObj in obj){
+            if(obj[constObj] !== elem[constObj]){
+                return false;
+            }            
+        }
+        return elem;
+    })
+    console.log(finalMass)
+  }
+
+  massObj(data, {position: "senior", isActive: false})
